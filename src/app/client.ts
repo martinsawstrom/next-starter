8569175e3f4ff3a5718c30955473dbde8836fba8
Martin Säwström <realelidooro@gmail.com>
2024-05-16 11:18:36 +0000

    test

--- src/app/client.ts
+++ src/app/client.ts
@@ -2,7 +2,7 @@ import { createThirdwebClient } from "thirdweb";
 
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
+const clientId =41e4394d2e90a19c2f768a60e80db413;
 
 if (!clientId) {
   throw new Error("No client ID provided");
